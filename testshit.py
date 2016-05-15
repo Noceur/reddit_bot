ed38b1b5a1155d8c95a6657e557ee8cb42085eee
Maximilian Mellhage <m.mellhage@gmail.com>
2016-05-15 16:32:00 +0200

    Function now removes from cache

--- testshit.py
+++ testshit.py
@@ -6,12 +6,12 @@ r = praw.Reddit(user_agent = "Simple bot for learning purposes by /u/dynamicstat
 
 print ("\n\n\n\n\n\n\n==============================")
 
-cache = [['d340aln', 'https://www.reddit.com/r/test/comments/4isyzj/test/d340aln', '1', '406429.76944444445'], ['d33kbo7', 'https://www.reddit.com/r/test/comments/4j3up9/test/d33kbo7', '1', '406418.7727777778'], ['d33k455', 'https://www.reddit.com/r/test/comments/4j3zk5/testing/d33k455', '1', '406418.6825']]
+cache = ['https://www.reddit.com/r/test/comments/4isyzj/test/d340aln', 'https://www.reddit.com/r/test/comments/4j3up9/test/d33kbo7', 'https://www.reddit.com/r/test/comments/4j3zk5/testing/d33k455',]
 
 
 
 
-commentremovalage = 10 #in hours
+commentremovalage = 1 #in hours
 
 
 
@@ -26,15 +26,21 @@ indexage					= indexid+3
 #cache.append(comment_age)
 
 def check_comments(cache):
-	for item in cache:
-		s = r.get_submission(item[indexlink])
+	for item in sorted(cache, reverse=True):
+		s = r.get_submission(item)
 		your_comment = s.comments[0]
 		if (((your_comment.created_utc)/60)/60+commentremovalage) <= ((time.time()/60)/60) : 
 			print ("comment is older than " + str(commentremovalage) + " hours and will therefor get removed")
+			cache.remove(your_comment.permalink)
 		else :
 			print ("comment not old enough to be deleted")
 		print (((your_comment.created_utc)/60)/60+commentremovalage)
 		print ((time.time()/60)/60)
+		#if to_remove : 
+			#for url in to_remove : 
+				#cache.remove(url)
+				#print (cache)
+
 		#if str(item[indexage]+120) <= str(your_comment.created_utc/60/60) :
 		#	print ("Removing comment" + your_comment.id)
 		#else : 
@@ -52,8 +58,8 @@ while True:
 	#run_bot(subtoget)
 	check_comments(cache)
 	time.sleep(0)
-	print ("comment id, comment permalink, comment score, comment unix age")
-	#for item in cache:
-		#print(item)
+	print ("\n\n==============================\ncomments in list")
+	for item in cache:
+		print(item)
 		#print(item[indexlink])
 	time.sleep(100)
