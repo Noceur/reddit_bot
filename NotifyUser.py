d533e8d4cd3ab8c9abb28ae4e0407cee219fa01e
Maximilian Mellhage <m.mellhage@gmail.com>
2016-05-16 21:48:28 +0200

    splitting up file in several pieces

--- NotifyUser.py
+++ NotifyUser.py
@@ -3,14 +3,20 @@ import smtplib
 
 
 def send_msg(SUBJECT, TEXT):
-	fromaddr = 'anotheruselessbot@gmail.com'
-	toaddrs  = 'dimoronen@gmail.com'
-	username = 'anotheruselessbot@gmail.com'
-	password = 'botbotbot'
-	convert_message
-	message = 'Subject: %s\n\n%s' % (SUBJECT, TEXT)
-	server = smtplib.SMTP('smtp.gmail.com:587')
+
+	fromaddr			= 'anotheruselessbot@gmail.com'
+	toaddrs  			= 'dimoronen@gmail.com'
+	username 			= 'anotheruselessbot@gmail.com'
+	password 			= 'botbotbot'
+	message 			= 'Subject: %s\n\n%s' % (SUBJECT, create_text(TEXT))
+	server 				= smtplib.SMTP('smtp.gmail.com:587')
+	#=====================================
+	#			STARTING SERVICE
+	#=====================================
 	server.starttls()
 	server.login(username,password)
 	server.sendmail(fromaddr, toaddrs, message)
 	server.quit()
+
+def create_text(TEXT):
+	
