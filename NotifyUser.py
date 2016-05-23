444def8110a94c16706a989e35b59b5a11a123a6
Maximilian Mellhage <m.mellhage@gmail.com>
2016-05-23 02:42:44 +0200

    Fixed crash, now uses str(item) for TEXT2.

--- NotifyUser.py
+++ NotifyUser.py
@@ -1,13 +1,15 @@
 import smtplib
+import time
 
+#cache_link			= ["abc1", "abc2", "abc3", "abc4", "abc5"]
+#cache_age			= ["1h", "2h", "3h", "4h", "5h"]
 
-
-def send_msg(SUBJECT, TEXT):
+def send_msg(SUBJECT, TEXT, TEXT2=[]):
 	fromaddr			= 'anotheruselessbot@gmail.com'
 	toaddrs  			= 'dimoronen@gmail.com'
 	username 			= 'anotheruselessbot@gmail.com'
 	password 			= 'botbotbot'
-	message 			= 'Subject: %s\n\n%s' % (SUBJECT, create_text(TEXT))
+	message 			= 'Subject: %s\n\n%s' % (SUBJECT, create_text(TEXT, TEXT2))
 	server 				= smtplib.SMTP('smtp.gmail.com:587')
 
 	#=====================================
@@ -19,9 +21,15 @@ def send_msg(SUBJECT, TEXT):
 	server.quit()
 	print (message)
 
-def create_text(TEXT):
+def create_text(TEXT, TEXT2):
 	item_total = ""
-		for item in TEXT:
-			global item_total
-			item_total += item + "\n"
-		return item_total
+	for item in TEXT:
+		item_total += item + "\n"
+		#item_total += TEXT2[item] + "\n"
+	for item in TEXT2:
+		item_total += str(item) + "\n"
+	return item_total
+
+#while True:
+	#send_msg("TESTING", cache_link, cache_age)
+	#time.sleep(100)
