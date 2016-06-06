4660f2ba9fe3626b59d02941d3b24032e91f8434
Maximilian Mellhage <m.mellhage@gmail.com>
2016-06-06 02:00:22 +0200

    added timestamp and loop numbering

--- CheckIP.py
+++ CheckIP.py
@@ -1,14 +1,21 @@
 import ipgetter
 import time
 import NotifyUser
+import datetime
 
 currentIP = ipgetter.myip()
+currentIP = "1.1.1.1"
+numberofloops = 0
 
 while True:
+	numberofloops += 1
+	#print ("check #" str(numberofloops) + "\n" + "At: " + str(datetime.datetime.now()))
+	print ("Loop #" + str(numberofloops) + "\n" + "At: " + str(datetime.datetime.now()))
+	print ("\nChecking IP")
 	if (currentIP == ipgetter.myip()):
 		print ("Ip is the same")
 	else:
-		print ("Ip has changed to " + ipgetter.myip())
+		print ('Ip has changed to "' + ipgetter.myip())
 		currentIP = ipgetter.myip()
 		NotifyUser.send_ip("IP changed", currentIP)
 	print ("sleeping 1200s")
