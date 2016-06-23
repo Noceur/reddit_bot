4799cc787de7a4c4055fb5fbe4bc65dc752fd191
Maximilian Mellhage <m.mellhage@gmail.com>
2016-06-23 04:10:41 +0200

    Added function to make sure directory exist and if not create it. OS independant.

--- FileM.py
+++ FileM.py
@@ -2,13 +2,19 @@ import pickle
 import os
 
 def save_file(filename, datatosave):
+	ensure_dir('data')
 	outfile = open(os.path.join(os.path.dirname(__file__), 'data', filename), "wb")
 	pickle.dump(datatosave, outfile)
 	outfile.close()
 	print (filename + " saved successfully.")
 
 def load_file(filename):
-	infile = open(os.path.join(os.path.dirname(__file__), 'data', filename), "rb")
+	infile = open(os.path.join(os.apyth.dirname(__file__), 'data', filename), "rb")
 	output = pickle.load(infile)
 	print (filename + " loaded successfully.")
 	return output
+
+def ensure_dir(f):
+	d = os.path.join(os.path.dirname(__file__), f)
+	if not os.path.exists(d):
+		os.makedirs(d)
